abc2dced4e78f4ec3e4aca39952363582a441232
dudor <dudor@qq.com>
2017-12-17 17:15:36 +0800

    add alias

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Hello from './component/Hello/Hello';
+import Hello from 'component/Hello/Hello';
+import { AppContainer } from "react-hot-loader";
+import getRouter from 'router/router'
 
-import getRouter from './router/router'
+/*初始化*/
+renderWithHotReload(getRouter());
 
-if(module.hot){
-  module.hot.accept()
+/*热更新*/
+if (module.hot) {
+  module.hot.accept('./router/router', () => {
+      const getRouter = require('./router/router').default;
+      renderWithHotReload(getRouter());
+  });
 }
 
-
-ReactDom.render(
-  getRouter(), document.getElementById('app')
-);
+function renderWithHotReload(RootElement) {
+  ReactDom.render(
+      <AppContainer>
+          {RootElement}
+      </AppContainer>,
+      document.getElementById('app')
+  )
+}
